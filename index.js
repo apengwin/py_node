06483f6c7e39bbd66c7ff83e50a835a41be51464
Allan Peng <potette007@gmail.com>
2017-04-17 16:31:48 -0700

    gonna try to untar

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * @param {function} The callback function.
  */
 const spawn = require('child-process-promise').spawn;
+const Storage = require('@google-cloud/storage');
 
 /*
 exports.testName = function helloGCS (event, callback) {
@@ -28,7 +29,7 @@ exports.testName = function helloGCS (event, callback) {
 */
 
 exports.HELLO = function HELLO (event, callback) {
-  var promise = spawn("echo $UID");
+  var promise = spawn("whoami");
   var childProcess = promise.childProcess;
   childProcess.stdout.on('data', function (data) {
     console.log('[spawn] stdouttttt: ', data.toString());
@@ -37,13 +38,43 @@ exports.HELLO = function HELLO (event, callback) {
     console.log('[spawn] stderr: ', data.toString());
   });
 
-
     promise.then(function(result) {
-        console.log(result.stdout.toString())
+        console.log(result.stdout.toString());
     })
     .catch(function(err) {
         console.error(err.stderr);
     }); 
   callback();
+}
+
+exports.lightweight_tar = function lightweight_tar (event, callback) {
+  const bucketName = "allanpywrentest";
+  const fileName = "lightweight.tar";
+
+  const storage = Storage();
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(fileName);
+  const options = {
+    destination: "/tmp/" + fileName
+  }
+  return file.download(options)
+    .then((err) => {
+      console.log(`File %{file.name} downloaded to ${dest].`);
+      var promise = spawn("tar -xvf --no-same-owner " + "/tmp/" + fileName);
+      var childProcess = promise.childProcess;
+      childProcess.stdout.on('data', function (data) {
+        console.log('[spawn] stdoutttt: ', data.toString());
+      });
+      childProcess.stderr.on('data', function (data) {
+        console.log('[spawn] stderr: ', data.toString());
+      });
+      promise.then(function(result) {
+        console.log("promise then");
+        console.log(result.stdout.toString());
+      }).catch(function(err) {
+        console.log("promise error");
+        console.err(err.stderr);
+      });
+  });
 }
 
