84af5f43473a0f5790d8716fbf64fc102c555f7b
Allan Peng <potette007@gmail.com>
2017-04-18 04:25:40 -0700

    readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 ## The code in this repo demonstrates that we can download a tarball from a gcloud storage bucket, and execute python scripts from it.
 
+How to reproduce results.
+### Uploading function
+
+From the command line, use `gcloud beta functions deploy lightweight_tar --stage-bucket <BUCKET_NAME> <TRIGGER>`
+
+where `<TRIGGER>` is `--trigger-bucket <STORAGE_BUCKET_NAME>`
+
+### calling function
+`touch test.txt && gsutil cp test.txt <STORAGE_BUCKET_NAME>`
+
+You can view the logs using the command `gcloud beta functions logs read --limit 20` or if you prefer html, on the [GCF console](https://console.cloud.google.com/functions/l)
+
+## Notes for posterity
+* GCF requires all functiosn return a promise, or end with a callback. Originally, I had problems of the functions running really slowly, but when I switched from returning promises to callbacks, everything inexplicably ran faster. The lesson here is to never return a promise, which sounds kinda dark when you think about it.
+
+* The function timeout and memory allocated return to default every time you deploy the function, and have to be reset manually.
+
+* The overhead of IPC between the child process and the parent printing out the child's logs is pretty significant, so it's probably a bad idea to print out the output of a `tar -xcf`. Plus it spams the logs.
--- index.js
+++ index.js
@@ -6,6 +6,83 @@
 const spawn = require('child-process-promise').spawn;
 const Storage = require('@google-cloud/storage');
 
+exports.lightweight_tar = function lightweight_tar (event, callback) {
+  const bucketName = "allanpywrentest";
+  const fileName = "condaruntime.tar.xz";
+
+  const conda_path = "/tmp/condaruntime/bin";
+  const storage = Storage();
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(fileName);
+  const dest = "/tmp/";
+  const options = {
+    destination: dest + fileName
+  }
+  console.log("starting");
+  file.download(options)
+    .then((err) => {
+      console.log(`File %{file.name} downloaded to ${dest}.`);
+
+      // tar without attempting to chown, because we can't chown.
+      var TAR = spawn("tar",  ["--no-same-owner", "-xzf", "/tmp/" + fileName, "-C", "/tmp"]);
+      var childProcess = TAR.childProcess;
+      console.log("Attempting to untar...");
+
+       /* uncommenting this is a bad idea. IPC slows down everything.
+       childProcess.stdout.on('data', function (data) {
+         console.log('[TAR] stdout: ', data.toString());
+       });
+       childProcess.stderr.on('data', function (data) {
+         console.log('[TAR] stderr: ', data.toString());
+       });
+       */
+
+       TAR.then(function() {
+         console.log("finished untarring");
+
+         // for record-keeping purposes, list the contents after /tmp after untarring.
+         var LS_SECOND = spawn("ls", ["-lha", conda_path]);
+         var secondChildProc = LS_SECOND.childProcess;
+
+         secondChildProc.stdout.on('data', function(data) {
+           console.log("[LS_after] stdout: ", data.toString());
+         });
+         secondChildProc.stderr.on('data', function(data) {
+           console.log("[LS_after] stderr: ", data.toString());
+         });
+
+         LS_SECOND.then(function() {
+           var attempt_python = spawn(conda_path + "/python", ["hello.py"]);
+           var pythonProc = attempt_python.childProcess;
+
+           pythonProc.stdout.on('data', function(data) {
+              console.log("[PYTHON] stdout: ", data.toString());
+           });
+           pythonProc.stderr.on('data', function(data) {
+             console.log("[PYTHON] stderr: ", data.toString());
+           });
+
+           attempt_python.catch(function(err) {
+             console.error("Python err: ", err);
+           });
+           callback();
+         });
+
+       }).catch(function(err) {
+         console.error('ERR: ', err);
+         callback(1);
+       });
+ //   });
+  }).catch(function(err) {
+    console.error("Error: ", err);
+    callback(1);
+  });
+}
+
+/* Utility functions to figure out what's going on under the hood, 
+ * since I haven't figured out how to ssh onto a GCF vm
+ */
+
 exports.neitzsche = function HELLO (event, callback) {
   // You are root. The ubermensch
   var promise = spawn("whoami");
@@ -26,89 +103,6 @@ exports.neitzsche = function HELLO (event, callback) {
   callback();
 }
 
-exports.lightweight_tar = function lightweight_tar (event, callback) {
-  const bucketName = "allanpywrentest";
-  const fileName = "condaruntime.tar.xz";
-
-  const conda_path = "/tmp/condaruntime/bin";
-
-  const storage = Storage();
-  const bucket = storage.bucket(bucketName);
-  const file = bucket.file(fileName);
-  const dest = "/tmp/";
-  const options = {
-    destination: dest + fileName
-  }
-  console.log("starting");
-  file.download(options)
-    .then((err) => {
-      console.log(`File %{file.name} downloaded to ${dest}.`);
-
-      //for record-keeping purposes, list the contents of /tmp
-     // var LS_FIRST = spawn("ls", ["-lha", "/tmp"]);
-     // var childProc = LS_FIRST.childProcess;
-  
-      /*
-      childProc.stdout.on('data', function (data) {
-        console.log("[LS] stdout: ", data.toString());
-      });
-      childProc.stderr.on('data', function (data) {
-        console.log("[LS] stderr: ", data.toString());
-      });
-      */
-      //LS_FIRST.then(function(result) {
-        // tar without attempting to chown, because we can't chown.
-        var TAR = spawn("tar",  ["--no-same-owner", "-xzf", "/tmp/" + fileName, "-C", "/tmp"]);
-        var childProcess = TAR.childProcess;
-        console.log("Attempting to untar...");
-        /* uncommenting this is a bad idea. IPC is super slow.
-        childProcess.stdout.on('data', function (data) {
-          console.log('[TAR] stdout: ', data.toString());
-        });
-        childProcess.stderr.on('data', function (data) {
-          console.log('[TAR] stderr: ', data.toString());
-        });
-        */
-        TAR.then(function() {
-          console.log("finished untarring");
-          // for record-keeping purposes, list the contents after /tmp after untarring.
-          var LS_SECOND = spawn("ls", ["-lha", conda_path]);
-          var secondChildProc = LS_SECOND.childProcess;
-
-          secondChildProc.stdout.on('data', function(data) {
-            console.log("[LS_after] stdout: ", data.toString());
-          });
-          secondChildProc.stderr.on('data', function(data) {
-            console.log("[LS_after] stderr: ", data.toString());
-          });
-
-          LS_SECOND.then(function() {
-            var attempt_python = spawn(conda_path + "/python", ["hello.py"]);
-            var pythonProc = attempt_python.childProcess;
- 
-            pythonProc.stdout.on('data', function(data) {
-              console.log("[PYTHON] stdout: ", data.toString());
-            });
-            pythonProc.stderr.on('data', function(data) {
-              console.log("[PYTHON] stderr: ", data.toString());
-            });
-
-            attempt_python.catch(function(err) {
-              console.error("Python err: ", err);
-            });
-            callback();
-          });
-
-        }).catch(function(err) {
-          console.error('ERR: ', err);
-          callback(1);
-        });
- //   });
-  }).catch(function(err) {
-    console.error("Error: ", err);
-    callback(1);
-  });
-}
 exports.list = function list(event, callback) {
   var LS_COMMAND = spawn("ls", ["-lha", "/tmp"]);
   var childProc = LS_COMMAND.childProcess;
