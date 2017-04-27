ae4f87221d039327b8d293f61bbc51da551feb70
Allan Peng <potette007@gmail.com>
2017-04-27 10:47:39 -0700

    added some rudimentary stuff

--- index.js
+++ index.js
@@ -6,19 +6,24 @@
 const spawn = require('child-process-promise').spawn;
 const Storage = require('@google-cloud/storage');
 
-exports.lightweight_tar = function lightweight_tar (event, callback) {
+exports.lightweight_tar = function lightweight_tar (req, res) {
   const bucketName = "allanpywrentest";
-  const fileName = "condaruntime.tar.xz";
+  const runtimeName = "condaruntime.tar.xz";
   const conda_path = "/tmp/condaruntime/bin";
   const storage = Storage();
   const bucket = storage.bucket(bucketName);
-  const file = bucket.file(fileName);
+  const runtime = bucket.file(runtimeName);
+
+  const func_filename = "/tmp/func.pickle";
+  const data_filename = "/tmp/data.pickle";
+  const output_filename = "/tmp/output.pickle";
+
   const dest = "/tmp/";
-  const options = {
+  options = {
     destination: dest + fileName
   }
   console.log("starting");
-  file.download(options)
+  runtime.download(options)
     .then((err) => {
       console.log(`File %{file.name} downloaded to ${dest}.`);
 
@@ -29,28 +34,50 @@ exports.lightweight_tar = function lightweight_tar (event, callback) {
 
       TAR.then(function() {
         console.log("finished untarring");
-        var attempt_python = spawn(conda_path + "/python", ["helper.py"]);
-        var pythonProc = attempt_python.childProcess;
+   
+        const func_bucket = storage.bucket(req.body.func_key[0]);
+        const func = func_bucket.file(req.body.func_key[1]);
+        options = {
+          destionation : func_filename
+        }
+        func.download(options)
+          .then((err) => {
+            const data_bucket = storage.bucket(req.body.data_key[0]);
+            const data = data_bucket.file(req.body.data_key[1]);
+            options = {
+              destionation: data_filename
+            }
+            data.download(options)
+              .then((err) => {
+                var attempt_python = spawn(conda_path + "/python", ["jobrunner.py", func_filename, data_filename, output_filename]);
+                var pythonProc = attempt_python.childProcess;
 
-        pythonProc.stdout.on('data', function(data) {
-          console.log("[PYTHON] stdout: ", data.toString());
-        });
-        pythonProc.stderr.on('data', function(data) {
-          console.log("[PYTHON] stderr: ", data.toString());
-        });
-
-        attempt_python.catch(function(err) {
-          console.error("Python err: ", err);
-        });
-          callback();
-       }).catch(function(err) {
-         console.error('ERR: ', err);
-         callback(1);
-       });
-  }).catch(function(err) {
-    console.error("Error: ", err);
-    callback(1);
-  });
+               pythonProc.stdout.on('data', function(data) {
+                 console.log("[PYTHON] stdout: ", data.toString());
+               });
+               pythonProc.stderr.on('data', function(data) {
+                 console.log("[PYTHON] stderr: ", data.toString());
+               });
+               attempt_python.catch(function(err) {
+                 console.error("Python err: ", err);
+               });
+               res.send("ok"); 
+               }).catch(function(err) {
+                 console.error('error downloading data: ', err);
+                 res.send("fail");
+             });
+           }).catch(function(err) {
+             console.error('ERR: ', err);
+             res.send("fail");
+           });
+        }).catch(function(err) {
+          console.error("Error: ", err);
+          res.send("fail");
+      });
+   }).catch(function(err) {
+     console.error("error downloading runtime");
+     res.send("fail");
+   });
 }
 
 /* Utility functions to figure out what's going on under the hood, 
@@ -86,6 +113,38 @@ exports.list = function list(event, callback) {
   });
   childProc.stderr.on('data', function (data) {
     console.log("[LS] stderr: ", data.toString());
+  });
+
+  LS_COMMAND.then(function(result) {
+    console.log(result.toString());
+  });
+}
+
+exports.OS = function list(event, callback) {
+  var LS_COMMAND = spawn("cat", ["/etc/issue"]);
+  var childProc = LS_COMMAND.childProcess;
+
+  childProc.stdout.on('data', function (data) {
+    console.log("[OS] stdout: ", data.toString());
+  });
+  childProc.stderr.on('data', function (data) {
+    console.log("[OS] stderr: ", data.toString());
+  });
+
+  LS_COMMAND.then(function(result) {
+    console.log(result.toString());
+  });
+}
+
+exports.CPU = function list(event, callback) {
+  var LS_COMMAND = spawn("lscpu", []);
+  var childProc = LS_COMMAND.childProcess;
+
+  childProc.stdout.on('data', function (data) {
+    console.log("[OS] stdout: ", data.toString());
+  });
+  childProc.stderr.on('data', function (data) {
+    console.log("[OS] stderr: ", data.toString());
   });
 
   LS_COMMAND.then(function(result) {
